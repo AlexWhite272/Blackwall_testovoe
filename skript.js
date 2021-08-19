f3f8dd60c92c7b019fd1115ceee0726d79adddb7
AlexWhite272 <272anon272@gmail.com>
2021-08-19 20:50:17 +0300

    first

---
+++ skript.js
@@ -0,0 +1,12 @@
+// const menu = document.querySelector(".nav__mobile")
+// const mobNav = document.querySelector(".mob__nav")
+// var open = false;
+// menu.addEventListener("click", ()=>{
+    
+//         mobNav.style.display = "block";
+
+    
+// })
+
+
+
